e9fbbbde2d14d9e150ca9d0629457b0f42a1df5b
divyang-27 <divyang.chhelavda.02@gmail.com>
2025-02-27 05:26:40 +0530

    cypress-tests2

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
       return config
